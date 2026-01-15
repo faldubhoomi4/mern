33f50aa2dc9f9e8e6b8ea6dc8c48db0836980038
unknown <faldubhoomi4@gmail.com>
2026-01-15 15:07:52 +0530

    lint staged

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 function welcome(name: string) {
   console.log('Welcome to the Auth Service, ${name}!');
+  
 }
 
 welcome('Developer');
+
+const user ={
+  name : "bhoomi"
+}
+
+console.log(user["name"])
